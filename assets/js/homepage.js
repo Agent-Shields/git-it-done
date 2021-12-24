7a5bdd221563d98421ad44a342122902f83cda8f
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2021-12-23 16:23:52 -0800

    code snapshot

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -53,16 +53,12 @@ var displayRepos = function(repos, searchTerm) {
 
   repoSearchTerm.textContent = searchTerm;
 
-
-
   // loop over repos
   for (var i = 0; i < repos.length; i++) {
     // format repo name
     var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-    var repo = repoName;
-    
-    // create a container for each repo
+    // create a link for each repo
     var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
     repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
