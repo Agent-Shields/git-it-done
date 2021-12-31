d06cc34a1990db796e59e328735f907097b931c3
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2021-12-31 12:52:06 -0800

    finish 6.5.7

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 var getFeaturedRepos = function(language){
   var apiUrl = "https://api.github.com/search/repositories?q=" + language + "is:featured&sort=help-wanted-issues";
@@ -15,7 +16,6 @@ var getFeaturedRepos = function(language){
       alert("Error: GitHub User Not Found")
     }
   });
-  
 }
 
 var formSubmitHandler = function(event) {
@@ -105,5 +105,18 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
+// button click handler
+var buttonClickHandler = function(event){
+  var language = event.target.getAttribute("data-language");
+  if (language) {
+    getFeaturedRepos(language);
+    
+      //clear old content 
+  repoContainerEl.textContent= "";
+  }
+}
+
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+languageButtonsEl.addEventListener("click", buttonClickHandler)
